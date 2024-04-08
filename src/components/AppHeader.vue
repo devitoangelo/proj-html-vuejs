6c6d5ff02f0866ab5530ce4d2e7e1a4e2852066b
devitoangelo <angelodevito4@gmail.com>
2024-04-08 18:55:49 +0200

    created ul and logo and compleate the style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,34 +1,72 @@
 <script>
 export default {
-    
+
 }
 </script>
 
-<template >
+<template>
     <!-- header -->
- <header id="header">
-    <!-- container -->
-    <div class="site_header">
+    <header id="header">
+        <!-- container -->
+        <div class="site_header">
+            <!-- logo -->
+            <img width="100px" src="/images/Logo.jpg" alt="">
+            <!-- lista header -->
+            <ul class="d-flex header_list ">
+                <li>
+                    <strong>Home</strong>
+                </li>
+                <li>
+                    <strong>About</strong>
+                    
+                </li>
+                <li>
+                    <strong>Academics</strong>
+                    
+                </li>
+                <li>
+                    <strong>Courses</strong>
+                    
 
-    </div>
-  </header>
+                </li>
+                <li>
+                    <strong>News</strong>
+                    
+
+                </li>
+            </ul>
+
+
+
+
+
+        </div>
+    </header>
 </template>
 
 
 
-<style >
-#header{
-background-color: red;
-width: 100%;
-height: 70px;
+<style>
+#header {
 
->.site_header{
-  background-color: black;
-  width: 70%;
-  height: 100%;
-  margin: auto;
-}
-}
+    width: 100%;
+    height: 70px;
 
-    
+    >.site_header {
+
+        width: 70%;
+        height: 100%;
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+
+        >.header_list {
+            list-style: none;
+            gap: 2rem;
+            align-items: center;
+
+        }
+
+    }
+}
 </style>
