d4f407b96f7bf5bcb7574f57898090642ba9be07
devitoangelo <angelodevito4@gmail.com>
2024-04-09 17:25:03 +0200

    change the img and add the button in jumbo

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 import AppHeader from './components/AppHeader.vue'
 
 
-export default{
-  components :{
+export default {
+  components: {
     AppHeader
   }
 }
@@ -12,7 +12,7 @@ export default{
 
 <template>
 
-  <AppHeader/>
+  <AppHeader />
 
 
 
@@ -20,8 +20,19 @@ export default{
     <!-- jumbo -->
     <div class="site_main">
       <div class="title">
+        <h1>Professional Development For <br>
+          Coaches And Leaders</h1>
+      </div>
+      <div class="ciao">
+        <button class="btn button">
+          Learn More
+        </button>
 
       </div>
+
+
+
+
 
 
 
@@ -39,30 +50,44 @@ export default{
 
 
 
- 
+
 
 </template>
 
 <style>
-.site_main{
+.site_main {
   width: 100%;
-  height: 1037px;
-  background-color: red;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-image: url(/images/jumbo.jpg);
+  height: 1000px;
+  background-image: url(/images/jumbo.png);
+  background-size: contain;
   background-repeat: no-repeat;
-  background-size: cover;
 
-
-  >.title{
-  background-color:black;
-    width: 50%;
-    height: 200px;
-
-    
+  >.title {
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30%;
+    font-size: xx-large;
   }
-}
 
+  >.ciao {
+    text-align: center;
+
+    >.button {
+      border: 1px solid #cccc;
+      border-radius: 0;
+      width: 150px;
+
+    }
+
+    >.button:hover {
+      border: 1px solid black;
+
+    }
+  }
+
+
+
+}
 </style>
